72cddb7a8ffba8b83c82e15adc87947afebdc9fe
Osip Akopiants <31482944+osipnunik@users.noreply.github.com>
2018-01-02 17:40:54 +0200

    add the comment

--- src/com/Akopiants/ArrayHolder.java
+++ src/com/Akopiants/ArrayHolder.java
@@ -2,7 +2,7 @@ package com.Akopiants;
 
 public class ArrayHolder {
 
-	private static final int array[][] = {
+	private static final int array[][] = {//this singleton is not been used and writed for demonstration, program use array from spring.xml
 				{0, 0, 0, 1, 0, 0},
 				{0, 0, 1, 1, 0, 0},
 				{0, 0, 0, 1, 0, 0}};
