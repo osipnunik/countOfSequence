d653f50a0e9fca4365644fc58da88fb0d89f91f5
Osip Akopiants <31482944+osipnunik@users.noreply.github.com>
2018-01-02 18:34:20 +0200

    use double checked locking

--- src/com/Akopiants/ArrayHolder.java
+++ src/com/Akopiants/ArrayHolder.java
@@ -1,13 +1,24 @@
 package com.Akopiants;
 
 public class ArrayHolder {
-
-	private static final int array[][] = {//this singleton is not been used and writed for demonstration, program use array from spring.xml
-				{0, 0, 0, 1, 0, 0},
-				{0, 0, 1, 1, 0, 0},
-				{0, 0, 0, 1, 0, 0}};
-
+	/*this singleton is not been used and writed for demonstration,
+	program use array from spring.xml*/
+	
+	private static int [][] array;
+	
+	private ArrayHolder(){}
+	
 	public static int[][] getArray() {
+		if (array==null){
+			synchronized (ArrayHolder.class) {
+				if (array==null){
+				array=new int[][]{
+					{0, 0, 0, 1, 0, 0},
+					{0, 0, 1, 1, 0, 0},
+					{0, 0, 0, 1, 0, 0}};
+				}
+			}
+		}
 		return array;
 	}
 	
